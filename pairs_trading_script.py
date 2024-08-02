f8e6e45d3bfc585bf7f39922091785cfcf243954
Frank <46886799@163.com>
2024-08-02 14:22:28 +0800

    fix:add pairs trading scripts

--- pairs_trading_script.py
+++ pairs_trading_script.py
@@ -1,10 +1,13 @@
 import requests
 from loguru import logger
 
-def requests_get(url: str, headers):
-    # 发送 GET 请求
-    response = requests.get(url, headers=headers,timeout=(30, 300))
 
+def requests_get(url: str, headers, params):
+    # 发送 GET 请求
+    if params is None:
+        response = requests.get(url, headers=headers, timeout=(30, 300))
+    else:
+        response = requests.get(url, params=params, headers=headers, timeout=(30, 300))
     # 检查响应状态
     if response.status_code == 200:
         logger.info("请求成功!")
@@ -29,32 +32,39 @@ def requests_post(url: str, headers, data):
 if __name__ == "__main__":
     headers = {
         'accept': 'application/json',
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNDY4ODY3OTlAMTYzLmNvbSIsImV4cGlyZXMiOjE3NTM1MjI2NjYuMTE3MDQ2fQ.yIeNVqU_A45hkRlLvbZ9QK82DUY_6g7E2DsOB2M8VU4',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNDY4ODY3OTlAMTYzLmNvbSIsImV4cGlyZXMiOjE3NTM2ODIxMzEuNDQ1NzM2Nn0.nHoiPQYeyOg5hnQ3NCgowSp5RHmBbHlSRtVhh3x0-JQ',
         'Content-Type': 'application/json',
     }
+    url_prefix = 'http://dreamtown.synology.me:5182'
     logger.info("***Step1:Hello World")
-    requests_get('http://dreamtown.synology.me:5181', headers)
+    requests_get(url_prefix, headers)
     logger.info("\n")
+
     logger.info("***Step2:Download Data")
-    download_data = {
-        "start_date": "2023-08-21",
-        "end_date": "2030-12-31",
-        "source": "akshare_stock",
-        "freq": "1d"
-    }
-    requests_post('http://dreamtown.synology.me:5181/grid_data', headers, download_data)
+    requests_get('{}/backtest/download_data'.format(url_prefix), headers)
     logger.info("\n")
+
     logger.info("***Step3:Generate Signals")
+    # strategy_type=cointegration&
+    # backtest_start=2023-01-01&backtest_end=2023-12-31&
+    # stk0=02359&stk1=603259&
+    # loss_limit=-0.1&
+    # lookback=40&
+    # enter_threshold=2.0&exit_threshold=0.5&init_cash=100000
     signal_data = {
-          "start_date": "2023-08-21",
-          "end_date": "2030-12-31",
-          "backtest_config": "grid_arrj_1d",
-          "strategy_name": "trend_following",
-          "grid_low": 6.25,
-          "grid_high": 10.35,
-          "grid_num": 6
+        "strategy_type": "cointegration",
+        "backtest_start": "2023-01-01",
+        "backtest_end": "2030-12-31",
+        "stk0": "02359",
+        "stk1": "603259",
+        "loss_limit": -0.1,
+        "lookback": 40,
+        "enter_threshold": 2.0,
+        "exit_threshold": 0.5,
+        "init_cash": 100000
     }
-    requests_post('http://dreamtown.synology.me:5181/grid/signals', headers, signal_data)
+    requests_get('{}/backtest/signals'.format(url_prefix), headers, signal_data)
     logger.info("\n")
+
     logger.info("***Step4:Hello World Again")
-    requests_get('http://dreamtown.synology.me:5181', headers)
+    requests_get(url_prefix, headers)
