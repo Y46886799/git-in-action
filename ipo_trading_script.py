15bb49b869d5e0c1bae7e466fcda22735ed14d7b
Frank <f.46886799@gmail.com>
2024-10-13 19:36:54 +0800

    Create ipo_trading_script.py

---
+++ ipo_trading_script.py
@@ -0,0 +1,23 @@
+from utils import *
+
+if __name__ == "__main__":
+    headers = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDY4ODY3OTlAMTYzLmNvbSIsImV4cGlyZXMiOjE3NTk5MTU5NzguNjU5MjV9.Id-bCCsi22NsmnhvYEhE5U0QWd91FFXBzspi18l_RUU',
+        'Content-Type': 'application/json',
+    }
+    base_url = "http://dreamtown.synology.me:8000"
+    logger.info("***Step1:Hello World,{}".format(get_current_time()))
+    requests_get(base_url, headers)
+    logger.info("\n")
+
+    logger.info("***Step2:Download Stock IPO Data")
+    requests_get('{}/current_stock_ipo'.format(base_url), headers)
+    logger.info("\n")
+
+    logger.info("***Step3:Participate Stock IPO")
+    requests_get('{}/auto_ipo'.format(base_url), headers)
+    logger.info("\n")
+
+    logger.info("***Step4:Hello World Again,{}".format(get_current_time()))
+    requests_get(base_url, headers)
