6832580f3a79d7e40758a8579e3bf949d4c09647
Y46886799 <y.46886799@gmail.com>
2025-06-01 11:49:48 +0800

    Update grid_trading_script.py

--- grid_trading_script.py
+++ grid_trading_script.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         'Content-Type': 'application/json',
     }
     logger.info("***Step1:Hello World,{}".format(get_current_time()))
+    logger.info('http://dreamtown.synology.me:5181')
     requests_get('http://dreamtown.synology.me:5181', headers)
     logger.info("\n")
 
@@ -17,6 +18,8 @@ if __name__ == "__main__":
         "source": "akshare_stock",
         "freq": "1d"
     }
+    logger.info('http://dreamtown.synology.me:5181/grid/data')
+    logger.info(download_data)
     requests_post('http://dreamtown.synology.me:5181/grid/data', headers, download_data)
     logger.info("\n")
 
@@ -31,6 +34,8 @@ if __name__ == "__main__":
           "grid_high": 10.35,
           "grid_num": 6
     }
+    logger.info('http://dreamtown.synology.me:5181/grid/signals')
+    logger.info(signal_data)
     requests_post('http://dreamtown.synology.me:5181/grid/signals', headers, signal_data)
     logger.info("\n")
 
@@ -45,6 +50,8 @@ if __name__ == "__main__":
           "grid_high": 10.35,
           "grid_num": 6
     }
+    logger.info('http://dreamtown.synology.me:5181/grid/signals')
+    logger.info(signal_data)
     requests_post('http://dreamtown.synology.me:5181/grid/signals', headers, signal_data)
     logger.info("\n")
 
@@ -59,6 +66,8 @@ if __name__ == "__main__":
           "grid_high": 10.35,
           "grid_num": 6
     }
+    logger.info('http://dreamtown.synology.me:5181/grid/signals')
+    logger.info(signal_data)
     requests_post('http://dreamtown.synology.me:5181/grid/signals', headers, signal_data)
     logger.info("\n")
 
@@ -73,6 +82,8 @@ if __name__ == "__main__":
           "grid_high": 10.35,
           "grid_num": 6
     }
+    logger.info('http://dreamtown.synology.me:5181/grid/signals')
+    logger.info(signal_data)
     requests_post('http://dreamtown.synology.me:5181/grid/signals', headers, signal_data)
     logger.info("\n")
 
@@ -87,6 +98,8 @@ if __name__ == "__main__":
           "grid_high": 10.35,
           "grid_num": 6
     }
+    logger.info('http://dreamtown.synology.me:5181/grid/signals')
+    logger.info(signal_data)
     requests_post('http://dreamtown.synology.me:5181/grid/signals', headers, signal_data)
     logger.info("\n")
 
@@ -101,8 +114,11 @@ if __name__ == "__main__":
           "grid_high": 10.35,
           "grid_num": 6
     }
+    logger.info('http://dreamtown.synology.me:5181/grid/signals')
+    logger.info(signal_data)
     requests_post('http://dreamtown.synology.me:5181/grid/signals', headers, signal_data)
     logger.info("\n")
 
     logger.info("***Step4:Hello World Again,{}".format(get_current_time()))
+    logger.info('http://dreamtown.synology.me:5181')
     requests_get('http://dreamtown.synology.me:5181', headers)
