12e256e020d277fabad405c941a4fe369000bd8b
Y46886799 <y.46886799@gmail.com>
2025-06-01 11:47:18 +0800

    Update incremental_download_data_script.py

--- incremental_download_data_script.py
+++ incremental_download_data_script.py
@@ -9,16 +9,20 @@ if __name__ == "__main__":
     }
     url_prefix = 'http://dreamtown.synology.me:5186'
     logger.info("***Step1:Hello World,{}".format(get_current_time()))
+    logger.info(url_prefix)
     requests_get(url_prefix, headers)
     logger.info("\n")
 
     logger.info("***Step2:Download Data")
+    logger.info('{}/incremental_download_data'.format(url_prefix))
     requests_get('{}/incremental_download_data'.format(url_prefix), headers)
     logger.info("\n")
 
     logger.info("***Step3:Download Data Weekend")
+    logger.info('{}/incremental_download_data_weekend'.format(url_prefix))
     requests_get('{}/incremental_download_data_weekend'.format(url_prefix), headers)
     logger.info("\n")
 
     logger.info("***Step4:Hello World Again,{}".format(get_current_time()))
+    logger.info(url_prefix)
     requests_get(url_prefix, headers)
